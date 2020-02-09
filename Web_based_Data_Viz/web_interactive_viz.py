d7d2828f3440fc9048cab83d783bd4e4ac3b7788
Miviemng <Mivie123hotmail.com>
2020-02-09 15:49:18 +0100

    Update web_interactive_viz.py

--- Web_based_Data_Viz/web_interactive_viz.py
+++ Web_based_Data_Viz/web_interactive_viz.py
@@ -27,7 +27,7 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 data_path = "Data.xlsx"
 data_dict, returns_df, dates = portfolio(data_path, 3)
-cov_matrix = returns_df.cov().round(4)
+cov_matrix = returns_df.corr().round(4)
 x = [name for name in cov_matrix]
 y = x
 
@@ -35,7 +35,7 @@ fig1 = ff.create_annotated_heatmap(z=cov_matrix.to_numpy(), x=x, y=y, showscale=
 
 fig1.update_layout(
     title={
-        'text': "Portfolio Covariance Matrix",
+        'text': "Portfolio Correlation Matrix",
         'y': 0.9,
         'x': 0.5,
         'xanchor': 'center',
@@ -53,7 +53,7 @@ colors = {
 app.layout = html.Div([
     html.Div(
         [html.H2(
-            children=['Data Visualization Assignment'],
+            children=['Data Visualization Assignment : Static view of our Interactive webpage'],
             style={
                 'color': "white",
                 'backgroundColor': colors['background']
@@ -61,15 +61,28 @@ app.layout = html.Div([
     ),
     html.Div(
         dcc.Markdown('''
-This web application that can be used to optimize a portfolio of assets and  display interactively the results.
+This Python web application can be used to optimize a portfolio of assets and display interactively the results. To get interactive plots the user should run our python scripts. But Installing python libraries can be difficult, to alleviate this problem we created a Software (binary file or .Exe) that runs on Windows [MIPOpti](https://github.com/MivieMng/Web-based-Interactive-visualization-of-Portfolio-optimization-results/releases).
+We used two visualization libraries [plotly](https://plot.ly/) and [dash](https://plot.ly/dash/), and one optimization library [cvxopt](https://cvxopt.org/).
+This webpage was entirely created using Dash, no html template was used.
 We downloaded the returns data of 10 stocks from [the website of the University of Pennsylvania](https://wrds-www.wharton.upenn.edu/).
-We used two visualization libraries [plotly](https://plot.ly/) and [dash](https://plot.ly/dash/) and one optimization library [cvxopt](https://cvxopt.org/).
 
  ***
 ''', style={'backgroundColor': 'white'})),
+    html.Div([html.H4(
+        children='Encountered difficulties:  ',
+        style={
+            'color': "black"}),
+        dcc.Markdown('''
+* Mastering Dash and plotly to produce high quality graphs
+  *  handling the properties of plot objects to render nice figures
+  * Advanced concepts like "Callbacks" : callbacks enable the update of figures as the inputs change
+  * Updating multiple figures by creating multiple callbacks
+  * Writing html instructions from Dash
+ ***
+''', style={'backgroundColor': 'white'})]),
     # dcc.Markdown("**weights**", id="Mark-down", style={"white-space": "pre"}),
     html.H4(
-        children='Inputs ',
+        children='User Inputs ',
         style={
             'color': "black"}),
     html.Label("Choose the stocks you want to analyze : use at least two"),
@@ -112,41 +125,58 @@ We used two visualization libraries [plotly](https://plot.ly/) and [dash](https:
     dcc.Markdown('''
  ***
 ''', style={'backgroundColor': 'white'}),
-    html.Div([
+    html.Div([html.H4(
+        children='Historical returns of a basket of assets',
+        style={
+            'color': "black"}),
         dcc.Graph(
             id='life-exp-vs-gdp',
         ),
+        html.Label(),
         dcc.Markdown('''
-            **Interpretation** : The monthly returns of the assets shows their performances. Assets with high variations can yield higher returns, but they are more volatile and therefore riskier.
+            
+            **Interpretation** : The monthly returns of the assets show their performances. Assets with high variations can yield higher returns, but they are more volatile and therefore riskier.
             
             ***
         ''', style={'backgroundColor': 'white'})]),
-    html.Div([
-    dcc.Graph(
-        figure=fig1,
-    ),
+    html.Div([html.H4(
+        children='Linear relationships between assets returns',
+        style={
+            'color': "black"}),
+        dcc.Graph(
+            figure=fig1,
+        ),
         dcc.Markdown('''
-                **Interpretation** : The covariance matrix highlights the correlations between each pair of assets in the portfolio. The higher the correlation the more the assets change (increase or decrease) in the same way.
+                **Interpretation** : The correlation matrix highlights the correlations (linear relationships) between the returns of all pair of assets in the portfolio. The higher the correlation the more the assets change (increase or decrease) in the same way.
+                A good portfolio should contain less correlated assets (close to zero or less than one). The correlation matrix is used as the main input in the optimization of a portfolio.
 
                 ***
             ''', style={'backgroundColor': 'white'})
     ]),
     html.Div([
-    dcc.Graph(
-        id='Eff',
-    ),
+        html.H4(
+        children='Solution of the optimization of a portfolio',
+        style={
+            'color': "black"}),
+        dcc.Graph(
+            id='Eff',
+        ),
         dcc.Markdown('''
-                **Interpretation** : The efficient frontier is the solution of the optimization of a portfolio. For each risk level the higher return is a point that belongs to the efficient frontier. The capital allocation line  can be used to determine how to 
+                **Interpretation** : The efficient frontier (blue discrete points on the figure) is the solution of the optimization of a portfolio. For each risk level the higher return is a point that belongs to the efficient frontier. The capital allocation line  can be used to determine how to 
                 share a bugdet between risky and a risk-free asset.
                 ***
             ''', style={'backgroundColor': 'white'})]
     ),
     html.Div([
+        html.H4(
+        children='Budget Allocation between risky and risk-free assets',
+        style={
+            'color': "black"}),
         dcc.Graph(
             id='bar',
         ),
-    dcc.Markdown('''
-                **Interpretation** : The allocation of a bugdet of 10k euros between risky and one risk free asset.
+        dcc.Markdown('''
+                **Interpretation** : The allocation of a bugdet of 10k euros between risky and one risk free asset. Assets with good risk-return (i.e. BR, CDN, BF, risk-free) trade-offs have positive allocation. 
                 
             ''', style={'backgroundColor': 'white'})]
     ),
@@ -174,7 +204,7 @@ def update_figure(stocks_names, risk_aversion):
         dict(
             x=data_dict['EF_10_with_short'][0],
             y=data_dict['EF_10_with_short'][1],
-            text="EF without short",
+            text="EF with short",
             mode='markers+line',
             opacity=1,
             marker={
@@ -194,7 +224,34 @@ def update_figure(stocks_names, risk_aversion):
                 'size': 11,
                 'line': {'width': 0.5, 'color': 'white'}
             },
-            name="Capital Allocation Line"
+            name="Capital Allocation Line with short"
+        )
+    ]
+    data_eff_no_short = [
+        dict(
+            x=data_dict['EF_10_without_short'][0],
+            y=data_dict['EF_10_without_short'][1],
+            text="EF with short",
+            mode='markers+line',
+            opacity=1,
+            marker={
+                'size': 13,
+                'line': {'width': 0.5, 'color': 'white'}
+            },
+            name="Efficient Frontier no short",
+            showlegend=True
+        ),
+        dict(
+            x=data_dict['CAL_10_stocks_without_short'][0],
+            y=data_dict['CAL_10_stocks_without_short'][1],
+            text="CAL no short",
+            mode='lines',
+            opacity=1,
+            marker={
+                'size': 11,
+                'line': {'width': 0.5, 'color': 'white'}
+            },
+            name="Capital Allocation Line no short"
         )
     ]
 
@@ -213,6 +270,7 @@ def update_figure(stocks_names, risk_aversion):
         ) for i in range(len(data_dict["mean_return"]))
     ]
 
+    # data_eff_all = stocks + data_eff+data_eff_no_short
     data_eff_all = stocks + data_eff
 
     return {
@@ -233,11 +291,16 @@ def update_figure(stocks_names, risk_aversion):
                ],
                'layout': dict(
                    xaxis={'title': 'date', 'tickformat': '%Y-%m-%d'},
-                   yaxis={'title': "returns"},
+                   yaxis={'title': "return", "size": 30},
                    # margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
                    legend={'x': 10, 'y': 1},
                    hovermode='closest',
-                   title="Portfolio Monthly returns"
+                   title="Portfolio Monthly returns",
+                   font=dict(
+                       # family="Courier New, monospace",
+                       size=18,
+                       # color="#7f7f7f"
+                   )
                )
            }, \
            {
@@ -279,13 +342,14 @@ def update_figure(stocks_names, risk_aversion):
                        name="negative allocation (or short sell)"
                    )
                ],
+
                'layout': dict(
                    xaxis={'title': 'stock'},
                    yaxis={'title': 'Capital Investment (euros) '},
                    # margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
                    # legend={'x': 10, 'y': 1},
                    hovermode='closest',
-                   title="Allocation of 10k€ between your stocks"
+                   title="Allocation of 10k€ between risky assets and one risk-free asset"
                )
            }
 
